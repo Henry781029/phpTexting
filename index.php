5c7fd24debc994f4b3e506e6bc9a7a7e04563bf1
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2020-10-08 18:19:24 -0500

    recursos de clase

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+    
+    
+    
+    </header>
+
+    <main>
+        <?php
+
+            /*$nombre = "pedro";
+            $edad = 29;
+            $ciudad = "medellin";
+            $identificacion = 102714946;
+            $nombre = "pablo"; 
+
+            
+            echo $nombre. "Tiene :".$edad ."Años";
+            $edad = 30;*/
+            
+            $costodolares = 25.5;
+            if($costodolares < 25.5 && $costodolares >20)
+            {
+                echo "no tiene descuento";
+            }
+            elseif ($costodolares >= 25.5)
+            {
+                echo "tiene desceunto";
+            }
+
+        ?>
+    
+    
+    
+    </main>
+
+    <footer>
+    
+    
+    
+    
+    </footer>
+    
+</body>
+</html>
